44cf897b73f0b86ce825f28541de3ab551ae9f67
Gurveer80 <sin10220@sheridancollege.ca>
2020-06-30 18:55:06 -0400

    fourth commit

---
+++ src/arthmetic/Arthmetic.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arthmetic;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gurveer Singh
+ */
+public class Arthmetic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter number");
+         int Input = s.nextInt(); 
+         
+         System.out.println("Enter Operation");
+         String Input2 = s.nextLine();
+         if(Input2=="max")
+                 {
+                     System.out.println("You Got it");
+                 }
+         else{
+                 System.out.println("Error");}
+         
+    
+        public enum Singh()
+                {
+            
+                         Add, Subtract,Multiplication
+              
+                }
+         Singh operation= Singh.Add;
+    }
+
+             
+    
+
+    
+
